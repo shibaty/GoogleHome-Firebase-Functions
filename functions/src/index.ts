b4bb80a69e266b46dfe907b7ae50f85ced4e6b72
shibaty <shibaty@gmail.com>
2018-01-28 01:03:43 +0900

    first commit

---
+++ functions/src/index.ts
@@ -0,0 +1,32 @@
+'use strict';
+
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp(functions.config().firebase);
+
+export const ElectronicsController = functions.https.onRequest((request, response) => {
+
+  const target: string = request.body.result.parameters.target;
+  const action: string = request.body.result.parameters.action;
+
+  console.log("target: " + target + " action: " + action);
+
+  (async () => {
+
+    const db = admin.database();
+    const ref = db.ref("electronics");
+    await ref.set({
+      target: target,
+      action: action
+    });
+  })();
+
+  response.setHeader("Content-Type", "application/json");
+  response.send(
+    JSON.stringify({
+      "speech": "できました",
+      "displayText": "できました"
+    })
+  );
+});
